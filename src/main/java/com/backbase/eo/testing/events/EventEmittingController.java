f5248521fe7733d8e70a14884db80c4d616d1173
oleksii <oleksii@backbase.com>
2021-04-14 13:21:15 +0300

    controller ready

--- src/main/java/com/backbase/eo/testing/events/EventEmittingController.java
+++ src/main/java/com/backbase/eo/testing/events/EventEmittingController.java
@@ -4,6 +4,7 @@ import com.backbase.buildingblocks.backend.communication.event.EnvelopedEvent;
 import com.backbase.buildingblocks.backend.communication.event.proxy.EventBus;
 import com.backbase.buildingblocks.persistence.model.Event;
 import com.backbase.buildingblocks.presentation.errors.BadRequestException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.lang.reflect.Field;
 import java.math.BigDecimal;
 import java.math.BigInteger;
@@ -15,10 +16,13 @@ import java.util.Optional;
 import java.util.Random;
 import java.util.Set;
 import java.util.UUID;
+import javax.annotation.Nullable;
 import org.reflections.Reflections;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,14 +30,19 @@ public class EventEmittingController {
 
     private Random random = new Random();
     private final EventBus eventBus;
+    private final ObjectMapper mapper;
 
     @Autowired
-    public EventEmittingController(EventBus eventBus) {
+    public EventEmittingController(EventBus eventBus, ObjectMapper mapper) {
         this.eventBus = eventBus;
+        this.mapper = mapper;
     }
 
-    @PostMapping(path="/events/{eventId}")
-    public void emitEvent(@PathVariable("eventId") String eventId) {
+    @PostMapping(
+        path="/events/{eventId}",
+        consumes = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public void emitEvent(@PathVariable("eventId") String eventId, @Nullable @RequestBody String body) {
         Reflections reflections = new Reflections("com.backbase");
         Set<Class<? extends Event>> availableEvents = reflections.getSubTypesOf(Event.class);
 
@@ -42,7 +51,11 @@ public class EventEmittingController {
             .findFirst()
             .flatMap(clazz -> {
                 try {
-                    return Optional.of(createAndFill(clazz));
+                    if(body != null) {
+                        return Optional.of(mapper.readValue(body, clazz));
+                    } else {
+                        return Optional.of(createAndFill(clazz));
+                    }
                 } catch (Exception e) {
                     e.printStackTrace();
                     return Optional.empty();
