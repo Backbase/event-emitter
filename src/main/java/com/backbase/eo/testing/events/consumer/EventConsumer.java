f577444173557a154a196fa279aa48a796b5fc45
Bogdan Zastavnyi <bogdan@backbase.com>
2022-11-10 17:31:53 +0100

    CEO-753: renamed EventConsumer

--- src/main/java/com/backbase/eo/testing/events/consumer/EventConsumer.java
+++ src/main/java/com/backbase/eo/testing/events/consumer/EventConsumer.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class CommunicationEventResolver {
+public class EventConsumer {
 
     private final EventStorage eventStorage;
 
